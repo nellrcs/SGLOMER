2da1a6bf23d2c69b4629fdc355ccd09cf6e57ed8
André Cristhian <web.cristian.ino@gmail.com>
2014-05-21 20:38:29 -0300

    Arrumei saporcaria do rodrigo

--- index.php
+++ index.php
@@ -3,10 +3,8 @@
 	require_once './class/Principal.class.php';
 	//include './controle/estrutura.php';
     
-<<<<<<< HEAD
-=======
+
 	echo Principal::dir_sistema();
 
->>>>>>> f9a4d041511ce800f698e0772696ad37146a09c6
 ?>
 <h1>HOLA</h1>
