241578fe96fa887aef9157cc2e189a72acefeeeb
André Cristhian <web.cristian.ino@gmail.com>
2014-05-21 20:14:38 -0300

    Adicionei meu nome de teste

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 	require_once './class/Principal.class.php';
-	include './controle/estrutura.php';
+	//include './controle/estrutura.php';
         
 ?>
+<h1>PAGINA INDEX ANDRÉ GOSTOSO</h1>
