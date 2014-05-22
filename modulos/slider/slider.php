d064b0b00db79b69d3ce70baef4c53d4cb3decf4
nellrcs <nellrcs@gmail.com>
2014-05-21 21:52:34 -0300

    Criar tabela Slider

--- modulos/slider/slider.php
+++ modulos/slider/slider.php
@@ -5,6 +5,21 @@
 
 	}
 	$ll = new slider();
+	
+	//Monstr aonde esta a class
 	$ll::onde_estou();
 
+	//cria tabela
+	$sql = "CREATE TABLE IF NOT EXISTS `slider` (
+		  `slider_id` int(11) NOT NULL AUTO_INCREMENT,
+		  `imagem` varchar(200) NOT NULL DEFAULT '0',
+		  `titulo` varchar(200) NOT NULL DEFAULT '',
+		  `descricao` text NOT NULL,
+		  PRIMARY KEY (`slider_id`)
+		) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COMMENT='Table with abuse reports' AUTO_INCREMENT=1;
+	";
+
+
+	$ll->criar_table($sql);	
+	//
 ?>
