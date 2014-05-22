8797237b56cde2ff1b0bcd10897c6bae3503dd34
Rodrigo Busch Ferreira <buschdead@gmail.com>
2014-05-21 21:27:11 -0300

    identaçao
    identaçao

--- class/Conexao.class.php
+++ class/Conexao.class.php
@@ -4,8 +4,12 @@
 	{
 		public function conecta(){
 			
-			$conn = mysql_connect(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD) or die (mysql_error());
+			$conn = mysql_connect(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD) or die(mysql_error());
 			$Select = mysql_select_db(DB_DATABASE) or die (mysql_error());
+		}
+
+		public function criar_table($string){
+			$quer = mysql_query($string) or die(mysql_error())
 		}
 	}
 
