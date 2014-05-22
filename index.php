e9575d412579e7796957edaaa538bf1af17d32c1
André Cristhian <web.cristian.ino@gmail.com>
2014-05-21 21:25:29 -0300

    Include no modulos/slider/slider.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-
 	require_once './class/Principal.class.php';
+
 	//include './controle/estrutura.php';
     
+	$diretorio_total = Principal::caminho_diretorio();
 
-	echo Principal::dir_sistema();
 
+	//SLIDER
+	include ('modulos/slider/slider.php');
 ?>
-<h1>HOLA</h1>
