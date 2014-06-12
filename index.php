865d2702dcd43c1ac0dcb1e80c5b9a10f2d401d0
nellrcs <nellrcs@gmail.com>
2014-06-12 14:59:30 -0300

    index
    plugins na pagina index

--- index.php
+++ index.php
@@ -1,8 +1,50 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<link rel="stylesheet" href="bootstrap-3.1.1/css/bootstrap.css">
+	<meta charset="UTF-8">
+	<title>Modulos</title>
+</head>
+<body>
 <?php
 	require_once './class/Principal.class.php';
+	/*
+		Layout exclusivo
+		Inserção de conteúdo inicial
+		Sistema de busca interna
+		Mapa de localização
+		Formulário de contato
+		Redes sociais (integração);
+		Imagens rotativas
+		Galeria de imagens ou vídeos
+		Integração ao Google
+		Integração ao Google Maps
+		Integração Google Analytics
+		Definição de palavras chaves
+		Responsividade
+		Chat online (Atendimento)
+		Sistema de gerenciamento
+		Enquetes
+		Mala direta (Newsletter)
+		Sistema de banners
+		Controle de usuários (Gerencial)
+		URL amigável
+		Sitemap em XML
+		Integração ao Google Webmaster
+		Formulário personalizável
+		Sistema de Blog
+		Catálogo de produtos
+		Comentário de usuários
+		Carrinho de pedidos
+		Controle de pedidos (Gerencial)
+		Relatório (Gerencial)
+	*/	
 
 	//include './controle/estrutura.php';
-    
+
+	//CONFIGURACAO DO SITE //
+	
+
 	$diretorio_total = Principal::caminho_diretorio();
 
 	//CRIA PASTA CENTRAL DE UPLOAD "uploads"
@@ -29,6 +71,7 @@
 	//GERA TABELA TEXTOS
 	include ('modulos/textos/textos.php');
 
-
-
 ?>
+
+</body>
+</html>
