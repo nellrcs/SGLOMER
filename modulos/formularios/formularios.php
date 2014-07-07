7a6c9dabf78a60f6514bff80198155e82825292c
nellrcs <nellrcs@gmail.com>
2014-07-07 00:41:08 -0300

    incluir metodo formulario no admin

--- modulos/formularios/formularios.php
+++ modulos/formularios/formularios.php
@@ -2,6 +2,7 @@
 <?php
 	class Objeto_formulario
 	{	
+              public $nome_posicao;
 		public $name;
 		public $tipo;
 		public $mask;
@@ -14,10 +15,10 @@
 
 	class Formularios extends Principal
 	{
+              public $id_pagina;
+
 		function montar_formulario(){
-
-			$form = new Principal();
-
+			
 			$sql = "CREATE TABLE IF NOT EXISTS `padrao_formulario` (
 					  `ID` int(11) NOT NULL AUTO_INCREMENT,
 					  `name` varchar(50) NOT NULL,
@@ -30,11 +31,12 @@
 					  `options` enum('nenhum','text','file') DEFAULT NULL,
 					  `label` varchar(255) NOT NULL,
 					  `ordem` int(5) NOT NULL,
+                                     `default` int(2) NOT NULL,
 					  PRIMARY KEY (`ID`)
 					) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COMMENT='Padrao de formularios' AUTO_INCREMENT=1;		
 			";
 
-			$form->slq_comando_insert($sql);
+			$this->slq_comando_insert($sql);
 
 			$sql = "CREATE TABLE IF NOT EXISTS `formulario` (
 				  `ID` int(11) NOT NULL AUTO_INCREMENT,
@@ -43,36 +45,75 @@
 				) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COMMENT='Padrao de formularios' AUTO_INCREMENT=1;		
 			";
 
-			$form->slq_comando_insert($sql);
+			$this->slq_comando_insert($sql);
 
-			$form->cria_diretorios_upload('formularios');
+			$this->cria_diretorios_upload('formularios');
 			
 		}
 
-		function __construct()
+		function __construct($id_pagina = null)
        	{
+                     $this->id_pagina = $id_pagina;
+                     $this->montar_formulario();
+       	}
+
+       	function slq_monta_form($posicao = '')
+       	{      		
+
+                     $campos = array('name','tipo','mask','maxlenth','opcoes_json','options','label','ordem');
+                     //$campos = array('name');
+                     $where = array('id_pagina'=>$this->id_pagina,'id_posicao'=>$posicao);
+
+                     $campos = $this->sql_select_otimizado('padrao_formulario',$campos,$where);
+
+                     //print_r($campos);
+                     foreach($campos as $campo){
+
+
+                            switch($campo['tipo']){
+                                   case 'input':
+                                          echo '<div>';
+                                          echo '<span>'.$campo['label'].'</span>';
+                                          echo '<input type="text" name="'.$campo['name'].'" maxlength="'.$campo['maxlenth'].'" required />';
+                                          echo '</div>';
+                                   break;
+
+                                   case 'select':
+                                          echo '<div>';
+                                          echo '<span>'.$campo['label'].'</span>';
+                                          //var_dump(explode(':',$campo->opcoes_json));
+                                          $var = $campo['opcoes_json'];
+                                          $var2 = json_encode($var);
+                                          //$var3 = json_decode($var);
+                                          var_dump($var);
+                                          echo '<select name="'.$campo['name'].'" required>';
+                                          
+                                          foreach($var as $chave => $value){
+                                                 echo '<option value="'.$chave['option'].'">'.$value['option'].'</option>';
+                                          }
+                                          echo '</select>';
+                                          echo '</div>';
+                                   break;
+
+                                   case 'textarea':
+                                          echo '<div>';
+                                          echo '<span>'.$campo['label'].'</span>';
+                                          echo '<textarea name="'.$campo['name'].'"></textarea>';
+                                          echo '</div>';
+                                   break;
+                            }
+
+
+                     }
 
        	}
 
-       	function slq_monta_form($query)
-       	{
+
+       	function criar_formulario($nome_posicao,$obj){
        		
+       		$sql = "INSERT INTO `padrao_formulario` (`ID`, `name`, `id_pagina`, `id_posicao`, `tipo`, `mask`, `maxlenth`, `opcoes_json`, `options`, `label`, `ordem`) VALUES (NULL, '$obj->name', '$this->id_pagina', '$nome_posicao', '$obj->tipo', '$obj->mask', '$obj->maxlenth', '$obj->opcoes_json', NULL,'$obj->label','$obj->ordem')";
 
-       		$sql = mysql_query($query);
-
-
-
-
-       		//return $campos;
-
-       	}
-
-
-       	function criar_formulario($id_pagina,$nome_posicao,$obj){
-       		
-       		$sql = "INSERT INTO `padrao_formulario` (`ID`, `name`, `id_pagina`, `id_posicao`, `tipo`, `mask`, `maxlenth`, `opcoes_json`, `options`, `label`, `ordem`) VALUES (NULL, '$obj->name', '$id_pagina', '$nome_posicao', '$obj->tipo', '$obj->mask', '$obj->maxlenth', '$obj->opcoes_json', NULL,'$obj->label','$obj->ordem')";
-
-       		echo $sql;
+       		//echo $sql;
 
        		$this->slq_comando_insert($sql);      		
        	}
@@ -82,14 +123,17 @@
        		$obj = new Objeto_formulario();
 
        		if(!empty($_POST)){
-       			$id_pagina = '15';
-       			$nome_posicao = 'AAAAA';
+       			$nome_posicao = 'BBBBB';
        			$obj->tipo = $_POST['tipo_formulario'];
+                            $obj->name = $_POST['name_formulario'];
+                            $obj->label = $_POST['label_formulario'];
        			$obj->mask = $_POST['mask_formulario'];
        			$obj->maxlenth = $_POST['maxlenth_formulario'];
        			$obj->opcoes_json = $_POST['opcoes_json_formulario'];
 
-       			$this->criar_formulario($id_pagina,$nome_posicao,$obj);
+                            $campos = array('id_pagina'=>$this->id_pagina,'id_posicao'=>$nome_posicao,'tipo'=>$obj->tipo,'name'=>$obj->name,'label'=>$obj->label,'mask'=>$obj->mask,'maxlenth'=>$obj->maxlenth,'opcoes_json'=>$obj->opcoes_json);
+
+       			$this->sql_insert_otimizado('padrao_formulario',$campos);
        		}
 
        		$inp = '<select name="tipo_formulario" required>';
@@ -103,6 +147,14 @@
        		$inp .= '</select>';
 
        		$inp .= '<br>';
+
+                     $inp .= '<input name="name_formulario" placeholder="Name" type="text" maxlenth="50" />';
+
+                     $inp .= '<br>';
+
+                     $inp .= '<input name="label_formulario" placeholder="Label" type="text" maxlenth="50" />';
+
+                     $inp .= '<br>';
 
        		$inp .= '<input name="mask_formulario" placeholder="MASK" type="text" maxlenth="50" />';
 
@@ -119,24 +171,36 @@
 
 
 
-       	function define_insere_formulario($id_pagina,$nome_posicao)
+       	function define_insere_formulario($nome_posicao)
        	{
-       		$this->montar_formulario();
 
-       		$sql = "SELECT * FROM formulario WHERE ID = ".$id_pagina." AND nome = '".$nome_posicao."'";
+                     $campos = array('ID','nome');
 
-       		if(!$this->slq_comando_select($sql, 1))
-			 {
-			 	$string ="INSERT INTO `formulario` (`ID`, `nome`) VALUES (NULL, '$nome_posicao')";
+                     $where = array('nome'=>$nome_posicao);
 
-			 	$ultimo_id = $this->slq_comando_insert($string);
+                     $for = $this->sql_select_otimizado('formulario',$campos,$where);
+
+                     // echo $for[0]['ID'];
+
+                     // echo $for[0]['nome'];
+
+       		if(count($for) <= 0)
+			{
+
+                            $campos =  array('nome' => $nome_posicao);
+                            
+                            $retorno_id = $this->sql_insert_otimizado('formulario',$campos);
+
+                            $ultimo_id = $retorno_id;
 
 
+			 	$obj = new Objeto_formulario();
 
+                            $obj->tipo = 'input';
 
-/*			 	$obj = new Objeto_formulario();
+                            $obj->name = 'nome';
 
-			 	$obj->tipo = 'input';
+                            $obj->label = 'Nome:';			 	
 
        			$obj->mask =  '';
        			
@@ -144,50 +208,49 @@
        			
        			$obj->opcoes_json = '';
 
-			 	$this->criar_formulario($id_pagina,$nome_posicao,$obj);*/
+                            $default = '1';
 
+			 	$this->criar_formulario($nome_posicao,$obj);
 
+                            $campos = array('nome');
+                            $where = array('ID'=>$ultimo_id,'nome'=>$nome_posicao);
+                            $nome_posicao = $this->sql_select_otimizado('formulario',$campos,$where);                            
 
-				echo $this->slq_monta_form("SELECT * FROM padrao_formulario WHERE id_posicao='$nome_posicao' AND id_pagina='$id_pagina'");
-
-
+				echo $this->slq_monta_form($nome_posicao[0]['nome']);
 
 			 }
 			 else
 			 {
-			 	echo   $this->slq_comando_select("SELECT * FROM padrao_formulario WHERE id_posicao='$nome_posicao' AND id_pagina='$id_pagina'");	
+			 	echo $this->slq_monta_form($for[0]['nome']);
 
 			 }
        	}
 	}
+/*
+    echo '<h4>FRONT FORMULARIO</h4>';
+    $id_pagina = '15';
+    $for = new Formularios($id_pagina);
+	$for->montar_formulario();*/
 
-
-	$for = new Formularios();
-	
-	$for->montar_formulario();
-
-	//FRONT
-	$for->define_insere_formulario(15,'AAAAA');
-
-
-
-
-
+//FRONT
+/*	 $for->define_insere_formulario('AAAAA');
+    echo '<br><br><br><br>';
+    $for->define_insere_formulario('BBBBB');
+    $for->slq_monta_form();*/
+ 	//echo 'Formulario';
 
 //BACK
-/*	$obj = new Objeto_formulario();
-
+/*    echo '<h4>BACK FORMULARIO</h4>';
+	$obj = new Objeto_formulario();
 	$obj->tipo = 'select';
 	$obj->mask = ' ';
 	$obj->maxlenth = '';
 	$obj->opcoes_json = '{option:1/NOME,2/SOBRENOME,3/CPF}';
 
 	echo '<form action="#" method="post">';
-
 	echo $for->formulario_back_test();
 	echo '<br>';
 	echo '<button name="submit" type="submit">ENVIAR</button>';
-
 	echo '</form>';*/
 
 
